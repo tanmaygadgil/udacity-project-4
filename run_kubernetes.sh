f4757dd2ad8606fffdf204a898a9307fee527cc6
tanmay-p-gadgil <tanmay-p-gadgil@pwc.com>
2020-06-13 00:09:33 +0530

    adding required files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath=tanmaygadgil/udacity-project-4:v2
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run udacity-project-3 --generator=deployment/apps.v1 --image=$dockerpath --port=80
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+echo "Sleeping for 100 seconds to complete the pull"
+sleep 100
+kubectl port-forward udacity-project-3 8080:80
--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,16 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath= tanmaygadgil/udacity-project-4
+
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+export DOCKER_ID_USER="tanmaygadgil"
+docker login
+docker tag udacity-project-4:v2 $DOCKER_ID_USER/udacity-project-4:v2
 
 # Step 3:
 # Push image to a docker repository
+docker push tanmaygadgil/udacity-project-4:v2
